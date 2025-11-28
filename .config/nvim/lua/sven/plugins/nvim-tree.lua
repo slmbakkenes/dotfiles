320df6af3b19e0ee8356784b135acf7d4f16858a
slmbakkenes <sven.bakkenes@monipath.com>
2025-11-28 10:46:50 +0100

    Update nvim-tree to be more intuative

--- .config/nvim/lua/sven/plugins/nvim-tree.lua
+++ .config/nvim/lua/sven/plugins/nvim-tree.lua
@@ -8,7 +8,23 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
+    -- Custom keymaps for nvim-tree buffer
+    local function on_attach(bufnr)
+      local api = require("nvim-tree.api")
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      -- Default mappings
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- Custom mappings for folder navigation
+      vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+      vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+    end
+
     nvimtree.setup({
+      on_attach = on_attach,
       view = {
         width = 35,
         relativenumber = true,
@@ -21,8 +37,17 @@ return {
         icons = {
           glyphs = {
             folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
+              arrow_closed = "", -- arrow when folder is closed
+              arrow_open = "", -- arrow when folder is open
+            },
+            git = {
+              unstaged = "M",
+              staged = "S",
+              unmerged = "U",
+              renamed = "R",
+              untracked = "?",
+              deleted = "D",
+              ignored = "◌",
             },
           },
         },
