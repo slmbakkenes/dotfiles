18b90bc27228e3536f30cb1227a5743cbb052be1
slmbakkenes <sven.bakkenes@monipath.com>
2025-12-02 17:39:12 +0100

    Updated omnisharp setup

--- .config/nvim/after/lsp/omnisharp.lua
+++ .config/nvim/after/lsp/omnisharp.lua
@@ -1,6 +1,12 @@
 return {
 	filetypes = { "cs" },
 	root_markers = { "*.slnx", "*.sln", "*.csproj", ".git" },
+	cmd = {
+		"omnisharp",
+		"--languageserver",
+		"--hostPID",
+		tostring(vim.fn.getpid()),
+	},
 	settings = {
 		FormattingOptions = {
 			EnableEditorConfigSupport = true,
